efcb70c44e969420d51607bb182f9effb172b96f
Richard Rivera <richard.rivera.roman@gmail.com>
2025-01-23 12:02:39 -0500

    create express server

---
+++ server/middlewares/errorMiddleware.ts
@@ -0,0 +1,38 @@
+import { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
+import type { ServerError } from '../types/types.ts';
+
+// 404 or “Not Found” Handler
+export const notFoundMiddleware = (
+  _req: Request,
+  _res: Response,
+  next: NextFunction,
+): void => {
+  const error: ServerError = {
+    log: '404 Not Found',
+    status: 404,
+    message: { err: '404 Not Found' },
+  };
+  next(error);
+};
+
+// 500 or “Global” Error handler
+export const errorHandler: ErrorRequestHandler = (
+  err: ServerError,
+  _req,
+  res,
+) => {
+  // Default error object
+  const defaultErr: ServerError = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+
+  // const errorObj: ServerError = Object.assign({}, defaultErr, err);
+  const errorObj: ServerError = { ...defaultErr, ...err };
+  // Log the error object
+  console.log('Error object:', errorObj);
+
+  res.status(errorObj.status).json(errorObj.message);
+};
+
